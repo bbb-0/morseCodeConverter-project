1ce546d6beda7eb4179b9c9685b0670910ccc665
bbb <mabracode@gmail.com>
2023-01-17 22:23:36 +1100

    remove old code

--- script.js
+++ script.js
@@ -37,62 +37,6 @@ const dictionary = {
 	' ': '/',
 };
 
-// 	const englishToMorse = (stringToTranslate, dictionaryObj) => {
-// 		return stringToTranslate
-// 			.toUpperCase()
-// 			.split('')
-// 			.map((char) => {
-// 				return dictionaryObj[char];
-// 			})
-// 			.join(' ');
-// 	};
-
-// 	if (type === 'encode') {
-// 		// Convert letters to morse code
-// 		let output = '';
-// 		input.split('').forEach((char) => {
-// 			if (morseCode[char.toLowerCase()])
-// 				output += morseCode[char.toLowerCase()] + ' ';
-// 			else output += char;
-// 		});
-// 		return output;
-// 	} else if (type === 'decode') {
-// 		// Convert morse code to letters
-// 		let output = '';
-// 		input.split(' ').forEach((code) => {
-// 			let letter = '';
-// 			for (let key in morseCode) {
-// 				if (morseCode[key] === code) {
-// 					letter = key;
-// 					break;
-// 				}
-// 			}
-// 			if (letter !== '') output += letter;
-// 			else if (code === '') output += '';
-// 			else output += code;
-// 		});
-// 		return output;
-// 	}
-// }
-
-// const input = document.querySelector('#input');
-// const output = document.querySelector('#output');
-
-// input.addEventListener('input', () => {
-// 	let inputValue = input.value;
-// 	if (/^[\s.-]+$/.test(inputValue)) {
-// 		output.value = convert(inputValue, 'decode');
-// 	} else {
-// 		output.value = convert(inputValue, 'encode');
-// 	}
-// });
-
-// const clearText = document.querySelector('#clear');
-// clearText.addEventListener('click', function () {
-// 	input.value = '';
-// 	output.textContent = '';
-// });
-
 const englishToMorse = (stringToTranslate, dictionaryObj) => {
 	return stringToTranslate
 		.toUpperCase()
