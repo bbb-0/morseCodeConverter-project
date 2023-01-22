bf1a8fa4f2c6d0ac040e595dbbc038fb568963d3
bbb <mabracode@gmail.com>
2023-01-22 16:33:11 +1100

    add comment in test

--- script.test.js
+++ script.test.js
@@ -2,6 +2,10 @@ import test from 'node:test';
 import { dictionary } from './js/dictionary.js';
 import { englishToMorse, morseToEnglish } from './js/morseUtils.js';
 
+// Add edge cases for non supported symbol/ char, what should the functios do in that case?
+// test for single char? for sentence? test different casing?
+// test the reverseObject function too
+
 describe('Morse code conversion functions', () => {
 	test('englishToMorse should convert English text to morse code', () => {
 		const englishText = 'HELLO WORLD';
